d89409fd05e47eafee5d4faf64e20edef50ce307
Anna Szymanska <ania01081944@o2.pl>
2024-12-08 16:35:11 +0100

    connecting with database

---
+++ src/main/java/pw/telm/telmbackend/TelmBackendApplication.java
@@ -0,0 +1,13 @@
+package pw.telm.telmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelmBackendApplication.class, args);
+    }
+
+}
